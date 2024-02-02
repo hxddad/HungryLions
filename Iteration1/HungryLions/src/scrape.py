0046caa4b28714a89aa5f12d06348de275d9c48f
jason <jasondeng107@gmail.com>
2024-02-01 23:11:11 -0500

    add webscrape and txt file database

--- Iteration1/HungryLions/src/scrape.py
+++ Iteration1/HungryLions/src/scrape.py
@@ -5,19 +5,33 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-name = soup.find_all('h2')
+restaurantName = soup.find_all('h2')
+card_divs = soup.find_all('div', class_='card border-0 mb-5')
 
-resturantName = []
-for names in name:
-    myname = {"name": names.text}
-    resturantName.append(myname)
+restaurantlist = []
+for names in restaurantName:
+     restaurantlist.append(names.text)
+
+tempdiv = []
+for card in card_divs:
+    restaurant = card.find_all('div', class_='row mb-3')
+    tempdiv.append(restaurant)
 
 
-location = soup.find_all("div", class_='col-11')
+info2 = []
+for x in range (len(tempdiv) - 1):
+    info = []
+    for z in range(len(tempdiv[x])):
+        info.append(tempdiv[x][z].text.strip())
+    info2.append(info)
+    
+
+f = open('restaurants.txt', 'w')
 
 
-f = open("text.txt", "w")
-i = 0
-for locations in location:
-    f.write(locations.text.strip() + "\n")
-    i+=1
+for x in range(len(restaurantlist)):
+    f.write(restaurantlist[x] + "   ")
+    for z in range(len(info2[x])):
+        f.write(info2[x][z] + "     ")
+    f.write("\n")
+
