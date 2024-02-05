ab6974fcb5505b686ae8694ed5d3eec0ecb6f514
Neel Upadhyay <150953795+NeelUpadhyay12@users.noreply.github.com>
2024-02-05 11:40:16 -0500

    uploaded review System

---
+++ Iteration1/HungryLions/src/ReviewSystemOutput.java
@@ -0,0 +1,177 @@
+package ReviewSys;
+
+import java.util.*;
+
+class ReviewSystem {
+
+    String username;
+    String reviewWords;
+    int foodQuality;
+    int service;
+    int atmosphere;
+    int valueForMoney;
+    int howmanyTimesGone;
+    int howManyFoodItemsEaten;
+
+    public ReviewSystem(String username, String reviewWords, int foodQuality, int service, int atmosphere, int valueForMoney,int howmanyTimesGone,int howManyFoodItemsEaten) {
+
+        this.username = username;
+        this.reviewWords = reviewWords;
+        this.foodQuality = foodQuality;
+        this.service = service;
+        this.atmosphere = atmosphere;
+        this.valueForMoney = valueForMoney;
+        this.howmanyTimesGone= howmanyTimesGone;
+        this.howManyFoodItemsEaten = howManyFoodItemsEaten;
+    }
+
+}
+
+class ReviewSystemFunctions {
+
+    private List<ReviewSystem> reviews;
+    private List<String> flagged;
+    private static final int WORD_LIMIT = 300;
+
+    ReviewSystemFunctions() {
+        this.reviews = new ArrayList<>();
+        this.flagged = new ArrayList<>();
+    }
+
+    public void submitReview(ReviewSystem review) throws SubmissionException {
+        if (moderationCheck(review.reviewWords) && wordLimit(review.reviewWords)&& ValidRating(review.foodQuality) && ValidRating(review.service)&& ValidRating(review.atmosphere) && ValidRating(review.valueForMoney)&& FormatCheck(review.reviewWords)&&TimesGone(review.howmanyTimesGone)&&numOfmenuItemsEaten(review.howManyFoodItemsEaten)) {
+            reviews.add(review);
+            System.out.println("Review submitted successfully!");
+            displayReviewDetails(review);
+        } else {
+            throw new SubmissionException("Review flagged as inappropriate, exceeds word limit, or contains invalid ratings. Not submitted.");
+        }
+    }
+
+    
+    private boolean FormatCheck(String reviewWords) {
+    	
+    	String[] reviewArray = reviewWords.split("\\s+");
+    	
+    	 for (String word : reviewArray) {
+           
+    		 if (word.equals(word.toLowerCase())) {
+                 return true;
+             }
+         }
+         
+         return false;
+    	
+     }
+    	
+    private boolean	TimesGone(int timesGone) {
+    	
+    	if(timesGone==0||timesGone<0) {
+    		return false;
+    	}
+    	else {
+    		return true;
+    	}
+    	
+    }
+    
+    private boolean numOfmenuItemsEaten(int foodItems) {
+    	
+    if(foodItems==0||foodItems<0)	{
+    	return false;
+    }
+    else {
+    	return true;
+    }
+    	
+    	
+    }
+    
+    private boolean moderationCheck(String reviewWords) {
+   
+        for (String flaggedPart : flagged) {
+            if (reviewWords.toLowerCase().contains(flaggedPart.toLowerCase())) {
+                return false; 
+            }
+        }
+        return true; 
+    }
+
+    private boolean wordLimit(String reviewWords) {
+        if( reviewWords.split("\\s+").length <= WORD_LIMIT) {
+        	return true;
+        }
+        	else {
+        		return false;
+        	}
+        
+    }
+
+    private boolean ValidRating(int rating) {
+       
+    	if(rating>=1 && rating <=5) {
+    		return true;
+    	}
+    	else {
+    	
+        return false;
+    	}
+    }
+
+    public void flagReview(String NotAllowedwords) {
+        flagged.add(NotAllowedwords);
+        System.out.println("Not Appropriate comment ");
+    }
+
+    public List<ReviewSystem> getReviews() {
+        return reviews;
+    }
+
+
+private void displayReviewDetails(ReviewSystem review) {
+    System.out.println("Review Details:");
+    System.out.println("Username: " + review.username);
+    System.out.println("Review Words: " + review.reviewWords);
+    System.out.println("Food Quality: " + review.foodQuality);
+    System.out.println("Service: " + review.service);
+    System.out.println("Atmosphere: " + review.atmosphere);
+    System.out.println("Value for Money: " + review.valueForMoney);
+    System.out.println("Times person went to the restaurant: " + review.howmanyTimesGone);
+    System.out.println("Number of menu items eaten: " + review.howManyFoodItemsEaten);
+   
+}
+
+public void displayAllReviews() {
+    System.out.println("All Reviews:");
+    for (ReviewSystem review : reviews) {
+        displayReviewDetails(review);
+    }
+}
+}
+class SubmissionException extends Exception {
+    public SubmissionException(String m) {
+        super(m);
+    }
+}
+
+public class ReviewSystemOutput {
+    public static void main(String[] args) {
+       
+    	 
+ }
+    	
+    	
+} 	
+    	
+    	
+
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+   
