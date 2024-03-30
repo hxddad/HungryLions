b4ecdb5ec2e75ff4089c68ec9530f6ef84795e00
Yazan Haddad <127855239+hxddad@users.noreply.github.com>
2024-03-30 08:01:56 -0400

    optimized DietaryFilter.java

--- Iteration1/HungryLions/src/DietaryFilter.java
+++ Iteration1/HungryLions/src/DietaryFilter.java
@@ -1,26 +1,15 @@
+package DietaryFilter;
 
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.File;  
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class DietaryFilter {
-	
-	public static void main(String[] args) throws FileNotFoundException {
-		listAll("restaurants.txt");
-		System.out.println("\n");
-		listHalal("restaurants.txt");
-		System.out.println("\n");
-		listKosher("restaurants.txt");
-		System.out.println("\n");
-		listNoGluten("restaurants.txt");
-		System.out.println("\n");
-		listVegan("restaurants.txt");
-		System.out.println("\n");
-		listVegetarian("restaurants.txt");
 		
-	}
-	
-	public static String listAll(String file) throws FileNotFoundException{
+	public static String listRestaraunts(String file) throws 
+	FileNotFoundException, NotADietaryRestriction{
 		File restaurants = new File(file);
 		 StringBuilder result = new StringBuilder();
 		 try (Scanner scanner = new Scanner(restaurants)) {
@@ -28,103 +17,40 @@ public class DietaryFilter {
 	                String line = scanner.nextLine();
 	                String[] col = line.split("   ");
 	                if (col.length >= 2) { 
-	                    result.append(col[0]).append(" [").append(col[1]).append("]").append("\n");
+	                    result.append(col[0]).append(" [").append(col[1])
+	                    .append("]").append("\n");
 	                }
 	            }
 	            return result.toString(); 
 	        } catch (FileNotFoundException e) {
-	            throw e;
+	            throw e;	        
+	   }
+	}
+	
+	public static String listRestaraunts(String file, String filter) throws 
+	FileNotFoundException, NotADietaryRestriction{
+		
+		 List<String> validFilters = Arrays.asList
+				 ("Halal (H)", "Kosher (K)", "Vegan (V)", "Vegetarian (VG)", "No Gluten (NG)");
+	      
+	        if (!validFilters.contains(filter)) {
+	            throw new NotADietaryRestriction("Not a dietary restriction.");
 	        }
-	}
 	
-    
-	public static String listHalal(String file) throws FileNotFoundException {
-		File restaurants = new File(file);
-		StringBuilder result = new StringBuilder();
-		 try (Scanner scanner = new Scanner(restaurants)) {
+	        File restaurants = new File(file);
+	        StringBuilder result = new StringBuilder();
+	        try (Scanner scanner = new Scanner(restaurants)) {
 	            while (scanner.hasNextLine()) {
 	                String line = scanner.nextLine();
-	                String[] col = line.split("   ");
-	              
-	                if (line.contains("Halal (H)") && col.length >= 2) {
-	                	result.append(col[0]).append(" [").append(col[1]).append("]").append("\n");
+	                String[] col = line.split("   "); 
+	                if (line.contains(filter) && col.length >= 2) { 
+	                    result.append(col[0]).append(" [").append(col[1])
+	                    .append("]").append("\n");
 	                }
 	            }
 	            return result.toString(); 
 	        } catch (FileNotFoundException e) {
-	            throw e;
-		}
-	}
-	
-	public static String listKosher(String file) throws FileNotFoundException {
-		File restaurants = new File(file);
-		StringBuilder result = new StringBuilder();
-		 try (Scanner scanner = new Scanner(restaurants)) {
-	            while (scanner.hasNextLine()) {
-	                String line = scanner.nextLine();
-	                String[] col = line.split("   ");
-	              
-	                if (line.contains("Kosher (K)") && col.length >= 2) {
-	                	result.append(col[0]).append(" [").append(col[1]).append("]").append("\n");
-	                }
-	            }
-	            return result.toString(); 
-	        } catch (FileNotFoundException e) {
-	            throw e;
-		}
-	}
-	
-	public static String listVegan(String file) throws FileNotFoundException {
-		File restaurants = new File(file);
-		StringBuilder result = new StringBuilder();
-		 try (Scanner scanner = new Scanner(restaurants)) {
-	            while (scanner.hasNextLine()) {
-	                String line = scanner.nextLine();
-	                String[] col = line.split("   ");
-	              
-	                if (line.contains("Vegan (V)") && col.length >= 2) {
-	                	result.append(col[0]).append(" [").append(col[1]).append("]").append("\n");
-	                }
-	            }
-	            return result.toString(); 
-	        } catch (FileNotFoundException e) {
-	            throw e;
-		}
-	}
-			
-	public static String listVegetarian(String file) throws FileNotFoundException {
-		File restaurants = new File(file);
-		StringBuilder result = new StringBuilder();
-		 try (Scanner scanner = new Scanner(restaurants)) {
-	            while (scanner.hasNextLine()) {
-	                String line = scanner.nextLine();
-	                String[] col = line.split("   ");
-	              
-	                if (line.contains("Vegetarian (VG)") && col.length >= 2) {
-	                	result.append(col[0]).append(" [").append(col[1]).append("]").append("\n");
-	                }
-	            }
-	            return result.toString(); 
-	        } catch (FileNotFoundException e) {
-	            throw e;
-		}
-	}
-	
-	public static String listNoGluten(String file) throws FileNotFoundException {
-		File restaurants = new File(file);
-		StringBuilder result = new StringBuilder();
-		 try (Scanner scanner = new Scanner(restaurants)) {
-	            while (scanner.hasNextLine()) {
-	                String line = scanner.nextLine();
-	                String[] col = line.split("   ");
-	              
-	                if (line.contains("No Gluten (NG)") && col.length >= 2) {
-	                	result.append(col[0]).append(" [").append(col[1]).append("]").append("\n");
-	                }
-	            }
-	            return result.toString(); 
-	        } catch (FileNotFoundException e) {
-	            throw e;
-		}
-	}
+	            throw e;	        
+	        }
+	    }
 }
